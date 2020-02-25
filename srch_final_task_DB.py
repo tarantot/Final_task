210839ab0e849b952a46950f8bf1ff5b7e6246e3
tarantot <60243020+tarantot@users.noreply.github.com>
2020-02-25 02:23:29 +0200

    Update srch_final_task_DB.py

--- srch_final_task_DB.py
+++ srch_final_task_DB.py
@@ -14,7 +14,7 @@ def srch_final_task_DB():
         regex = re.compile(result)
         match = re.search(regex, inputfile)
         if match:
-            print('Found "{}" in "{}"'.format(result, inputfile))
+            print('Found "{}" in "{}"'.format(result))
             text_pos = match.span()
             print(text[match.start():match.end()])
             resultfile.write(results)
