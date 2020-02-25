34fac6fa01a0a17652af058d5030dd214ee13500
tarantot <60243020+tarantot@users.noreply.github.com>
2020-02-25 14:10:53 +0200

    Update srch_final_task_DB_execution.py

--- srch_final_task_DB_execution.py
+++ srch_final_task_DB_execution.py
@@ -2,5 +2,7 @@ import srch_final_task_DB
 import srch_final_task_DB_1
 
 print('\n\t\t\t* * * * * SEARCH MODE * * * * * ')
+srch_final_task_DB.srch_final_task_DB()
+srch_final_task_DB_1.srch_final_task_DB()
 srch_parameter = input('by \nFULL NAME\n / \nPHONE NUMBER\n: ')
 srch = input('Please type the info to search: ')
